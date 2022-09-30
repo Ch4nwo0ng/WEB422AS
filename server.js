9a79370b48ee8c573676491d36b4eee63d282ebf
Chan <cwpark1215@gmail.com>
2022-09-30 02:34:38 -0400

    COMMITFORA2

--- server.js
+++ server.js
@@ -25,54 +25,79 @@ app.get("/",(req,res)=>{
 });
 
 //add new movie
-app.post("/api/movies", (req,res)=>{
+app.post("/api/movies", async (req,res)=>{
     
-    db.addNewMovie(req.body).then((data)=>{
-        res.json(data);
-        res.status(201).json({message: "new movie created"});
-    }).catch(()=>{
+    try{
+        let movie = await db.addNewMovie(req.body);
+        res.status(201).json(movie);
+    }
+    catch(err){
         res.status(500).json({message: "Error: Request is failed!!"});
-    });
-
+    }
 });
 
 //get movie with page & perPage
-app.get("/api/movies", (req,res)=>{
+app.get("/api/movies", async (req,res)=>{
     
-    db.getAllMovies(req.query.page,req.query.perPage,req.query.title).then((data)=>{
-        res.json(data);
-    }).catch(err=>{
-        res.status(500).json({message: err});
-    });
-   
+    try{
+        let movie = await db.getAllMovies(req.query.page,req.query.perPage,req.query.title);
+        res.status(200).json({movie});
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }   
 });
 
 //get movie with id
-app.get("/api/movies/:id", (req,res)=>{
-    db.getMovieById(req.params.id).then((data)=>{
-        res.json(data);
-    }).catch(()=>{
-        res.status(500).json({message: "Error: Unavailable movie Id!!"});
-    });
+app.get("/api/movies/:id", async (req,res)=>{
+
+    try{
+        let movie = await  db.getMovieById(req.params.id);
+        if(movie){
+            res.json(movie);
+        }
+        else{
+            res.status(404).json({error: 'Not exist'});
+        }
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
+    
 });
 
 //update movie with id
-app.put("/api/movies/:id", (req,res)=>{
-    db.updateMovieById(req.params.id, req.body).then((data)=>{
-        res.json(data);
-        res.status(204).json({message: `name with id: ${req.params.id} updated`});       
-    }).catch(()=>{
-        res.status(500).json({message: "Error: Unavailable movie Id!!"});
-    });
+app.put("/api/movies/:id", async (req,res)=>{
+
+    try{
+        let updateMovie = await db.updateMovieById(req.params.id, req.body);
+        if(!updateMovie.modifiedCount){
+            res.status(404).json({message: "Can not update!!"});
+        }
+        else{
+            res.status(204).json({message: `name with id: ${req.params.id} updated`});
+        }
+    }
+    catch(err){
+        res.status(500).json({message: "Error: Unavailable movie Id!!"})
+    }    
 });
 
 //delete movie with id
-app.delete("/api/movies/:id", (req,res)=>{
-    db.deleteMovieById(req.params.id, req.body).then(()=>{
-        res.json({message: `deleted name with id: ${req.params.id}`});
-    }).catch(()=>{
+app.delete("/api/movies/:id", async (req,res)=>{
+
+    try{
+        let movie = await db.deleteMovieById(req.params.id, req.body);
+        if(!movie.deletedCount){
+            res.status(404).json({message: "Can not delete"});
+        }
+        else{
+            res.status(204).json({message: `deleted name with id: ${req.params.id}`});
+        }
+    }
+    catch(err){
         res.status(500).json({message: "Error: Unavailable movie Id!!"});
-    });
+    }   
 });
 
 db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
